c9e0e205db2dc7df523848d27a5771c96b8a65b0
Terra <bclaycamp@gmail.com>
2021-10-03 16:33:44 -0400

    BaseFiles

---
+++ script.js
@@ -0,0 +1,6 @@
+const weatherAPIkey = "5d578c737ce21d8b0f9dd6879574a1b6";
+
+//https://swapi.dev/
+
+//get and store user's location
+// api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
