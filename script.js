5c309350926b1e73f51cf6a54dd20d1a0098d57a
Terra <you@example.com>
2021-10-03 18:01:03 -0400

    Got the button conected and atempting fetch

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ const weatherAPIkey = "5d578c737ce21d8b0f9dd6879574a1b6";
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 const button = document.querySelector("#search");
-button.addEventListener("click", () => {// i make the button clicky
-  const inputTitle = document.getElementById("blank").value;
-
-  fetch(`${BASE_URL}s=${inputTitle}`)
+button.addEventListener("click", () => {        // i make the button clicky
+  const inputCity = document.getElementById("boxie").value;
+    //console.log(fetch);
+  fetch(`api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${weatherAPIkey}`)
     .then((res) => {
+        console.log(res);
       return res.json();
     })
     .then((resJSON) => {
       console.log(resJSON);
-      renderList(resJSON.Search);
     })
     .catch((error) => {
       console.log(`**ERROR**: ${error}`);
     }); 
- );
+});
