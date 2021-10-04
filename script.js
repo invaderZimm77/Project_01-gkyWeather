0aee40fbcc33163fe284febb5fca06de20e803c7
Terra <you@example.com>
2021-10-04 10:41:36 -0400

    cleard some old test code

--- script.js
+++ script.js
@@ -13,17 +13,13 @@ button.addEventListener("click", () => {        // i make the button clicky
     console.log(inputCity);
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${weatherAPIkey}&`)
-    
     .then((res) => {
         console.log(res);
-        console.log("fuck");
         return res.json();
     })
-    
     .then((resJSON) => {
         console.log(resJSON);
     })
-    
     .catch((error) => {
         console.log(`**ERROR**: ${error}`);
     }); 
