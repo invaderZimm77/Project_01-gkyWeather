b71bdb7e5ab89f1016cb7596a82af317080d9bc1
Terra <you@example.com>
2021-10-04 09:56:44 -0400

    got it returning the searched city

--- script.js
+++ script.js
@@ -9,17 +9,22 @@ const weatherAPIkey = "5d578c737ce21d8b0f9dd6879574a1b6";
 
 const button = document.querySelector("#search");
 button.addEventListener("click", () => {        // i make the button clicky
-  const inputCity = document.getElementById("boxie").value;
-    //console.log(fetch);
-  fetch(`api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${weatherAPIkey}`)
+    const inputCity = document.getElementById("boxie").value;
+    console.log(inputCity);
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${weatherAPIkey}&`)
+    
     .then((res) => {
         console.log(res);
-      return res.json();
+        console.log("fuck");
+        return res.json();
     })
+    
     .then((resJSON) => {
-      console.log(resJSON);
+        console.log(resJSON);
     })
+    
     .catch((error) => {
-      console.log(`**ERROR**: ${error}`);
+        console.log(`**ERROR**: ${error}`);
     }); 
 });
