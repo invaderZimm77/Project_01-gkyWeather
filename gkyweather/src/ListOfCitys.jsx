f501cc80b8f556df39e3f2b572b2558d32019694
Terra <bclaycamp@gmail.com>
2022-06-09 10:33:18 -0400

    css styling of city tile and city list

--- gkyweather/src/ListOfCitys.jsx
+++ gkyweather/src/ListOfCitys.jsx
@@ -1,7 +1,8 @@
 import CityTile from "./CityTile"
-
 import React from 'react'
 
+
+//set state of ListOfCitys, adding the new
 const ListOfCitys = () => {
   return (
     <section className="list-of-cities">
