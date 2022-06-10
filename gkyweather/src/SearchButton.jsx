970babc53fb39d218879eebf4a6f700e671d19b3
Terra <bclaycamp@gmail.com>
2022-06-10 13:07:32 -0400

    css styling of search section, city tile and city tile list

--- gkyweather/src/SearchButton.jsx
+++ gkyweather/src/SearchButton.jsx
@@ -2,9 +2,8 @@ import GKYplanetPicker from "./Functions/GKYplanetPicker";
 import ListOfCitys from "./ListOfCitys";
 
 export default function SearchButton() {
-  //bringing in the list of cities
-  //verfying that the querey isn't already in the list city
-  //search city an add to list
+   //verfying that the querey isn't already in the list city
+ //add searched city  to list. update state.
 
   return <button className="search-button">Search</button>;
 }
